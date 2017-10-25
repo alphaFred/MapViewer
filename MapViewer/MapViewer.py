397ebc21667f8fa10e679b731b5701987f11936d
alphaFred <alphaFred@users.noreply.github.com>
2017-10-25 20:32:16 +0200

    Implemented additional cmd arguments
    - Implemented output file argument
    - Implemented output format argument (default: html)

--- MapViewer/MapViewer.py
+++ MapViewer/MapViewer.py
@@ -4,12 +4,20 @@ from MapParser import MapParser
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Parse .map-file')
-    parser.add_argument("map_file", metavar="f", type=str,
+    parser.add_argument("map_file", metavar="i", type=str,
                         help="file path to .map-file")
+    parser.add_argument("out_file", metavar="o", type=str,
+                        help="file path to output-file")
+    parser.add_argument("-f", "--format", type=str,
+                        help="format of output-file", default="html")
 
     args = parser.parse_args()
 
     m_parser = MapParser()
     with open(args.map_file, 'r') as map_file:
         m_parser.parse(map_file)
+
+    if args.format == "html":
+        m_parser.to_html(args.out_file)
+
     print("Finished parsing")
