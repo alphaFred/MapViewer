03beefbe14d1975b8edb74d46a77b849ab587a17
alphaFred <alphaFred@users.noreply.github.com>
2017-10-28 23:25:41 +0200

    Refactored to_html formatter

--- MapViewer/MapParser/MapParser.py
+++ MapViewer/MapParser/MapParser.py
@@ -83,26 +83,60 @@ class MapParser(object):
             self.sections.append(self.latestSection)
             self.latestSection = Section()
 
+    @staticmethod
+    def out_file_writeln(out_file, content, indentation):
+        indent = "\t" * indentation
+        out_file.write(indent + content + "\n")
+
     def to_html(self, out_file_path):
         colors = ['9C9F84', 'A97D5D', 'F7DCB4', '5C755E']
-        total_height = 32.0
+        indent = 0
+        total_height = 64.0
         total_size = len(self.sections[0])
 
         with open(out_file_path, "w") as out_file:
-            out_file.write("<html><head>")
-            out_file.write(
-                "<style>a { color: black; text-decoration: none; font-family:"
-                "monospace }</style>")
-            out_file.write("<body>")
-            out_file.write("<table cellspacing='1px'>")
-            for i, sec in enumerate(self.sections[1:]):
-                height = (total_height / total_size) * len(sec)
-                font_size = 1.0 if height > 1.0 else height
-                out_file.write(
-                    "<tr style='background-color:#%s;height:%gem;line-height:"
-                    "%gem;font-size:%gem'><td style='overflow:hidden'>" % (
-                        colors[i % len(colors)], height, height, font_size))
-                out_file.write("<a href='#%s'>%s</a>" % (str(sec), str(sec)))
-                out_file.write("</td></tr>")
-            out_file.write("</table>")
-            out_file.write("</body></html>")
+            self.out_file_writeln(out_file, "<html>", indent)
+            indent += 1
+            self.out_file_writeln(out_file, "<head>", indent)
+            self.out_file_writeln(out_file, "<style>a { color: black; "
+                                            "text-decoration: none; "
+                                            "font-family:monospace }</style>",
+                                  indent)
+            self.out_file_writeln(out_file, "<body>", indent)
+            indent += 1
+            for section in self.sections:
+                self.out_file_writeln(out_file, "<table cellspacing='1px'>",
+                                      indent)
+                indent += 1
+                self.out_file_writeln(out_file, "<thead>", indent)
+                indent += 1
+                self.out_file_writeln(out_file, "<tr>", indent)
+                indent += 1
+                self.out_file_writeln(out_file,
+                                      "<th>{0}</th>".format(section.section),
+                                      indent)
+                indent -= 1
+                self.out_file_writeln(out_file, "</tr>", indent)
+                indent -= 1
+                self.out_file_writeln(out_file, "</thead>", indent)
+                self.out_file_writeln(out_file, "<tbody>", indent)
+
+                for i, sec in enumerate(section.sub_sections):
+                    height = (total_height / total_size) * len(sec)
+                    font_size = 1.0 if height > 1.0 else height
+                    cell_color = colors[i % len(colors)]
+
+                    indent += 1
+                    self.out_file_writeln(out_file, "<tr style='background-color:#{color};height:{height}em;line-height:{height}em;font-size:{font_size}em'>".format(color=cell_color,height=height,font_size=font_size), indent)
+                    indent += 1
+                    self.out_file_writeln(out_file, "<td style='overflow:hidden'>{name}</td>".format(name=str(sec)), indent)
+                    indent -= 1
+                    self.out_file_writeln(out_file, "</tr>", indent)
+                    indent -= 1
+                self.out_file_writeln(out_file, "</tbody>", indent)
+                indent -= 1
+                self.out_file_writeln(out_file, "</table>", indent)
+            indent -= 1
+            self.out_file_writeln(out_file, "</body>", indent)
+            indent -= 1
+            self.out_file_writeln(out_file, "</html>", indent)
